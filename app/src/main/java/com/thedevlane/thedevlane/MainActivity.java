8157c7afc35860106054daf0654fb03cd3f672a0
Ant C <anthonyrobertcook@gmail.com>
2019-05-29 23:35:18 +0100

    removal of notification - validation attempt #1 failed

--- app/src/main/java/com/thedevlane/thedevlane/MainActivity.java
+++ app/src/main/java/com/thedevlane/thedevlane/MainActivity.java
@@ -24,38 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void SendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-        createNotificationChannel();
+        EditText uName = findViewById(R.id.username);
+        EditText pWord = findViewById(R.id.password);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "chan_1")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("theDevLane")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, builder.build());
-
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.edit_message);
-            String description = getString(R.string.edit_message);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("chan_1", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+        if (uName.getText().toString() != "@"){
+            uName.setHint("Not a valid Email Address");
         }
+        if ((uName.getText().toString() != "") & (pWord.getText().toString() != "")) {
+            intent.putExtra(EXTRA_MESSAGE, "hello moto!");
+            startActivity(intent);
+        }
+
     }
+
 }
